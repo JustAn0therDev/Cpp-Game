9c41f13b7832fda596c80018bef16560d3cabfb0
Ruan Montelo <ruanmontelo@gmail.com>
2022-09-23 14:23:31 -0300

    Adding movement to the circle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+const float MOVE_BY = 20.0f;
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     sf::CircleShape shape(50.f);
@@ -9,10 +11,26 @@ int main() {
     while (window.isOpen())
     {
         sf::Event event;
+        sf::Vector2f offset(0.0f, 0.0f);
+
         while (window.pollEvent(event))
         {
-            if (event.key.code == sf::Keyboard::Enter) {
-                shape.setRadius(shape.getRadius() + 5.0f);
+            switch (event.key.code)
+            {
+                case sf::Keyboard::Up:
+                    shape.move(0, -MOVE_BY);
+                    break;
+                case sf::Keyboard::Down:
+                    shape.move(0, MOVE_BY);
+                    break;
+                case sf::Keyboard::Left:
+                    shape.move(-MOVE_BY, 0);
+                    break;
+                case sf::Keyboard::Right:
+                    shape.move(MOVE_BY, 0);
+                    break;
+                default:
+                    break;
             }
 
             if (event.type == sf::Event::Closed)
